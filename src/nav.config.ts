30305b8846bf3fd8ac853675409b835e537029dd
Ryan Cartwright <fryzee.cartwright@gmail.com>
2024-08-22 13:41:06 +1000

    wip flutter blog

--- src/nav.config.ts
+++ src/nav.config.ts
@@ -548,6 +548,10 @@ const fullNav: FullNav = {
           title: 'REST API',
           href: '/guides/dart/serverless-rest-api-example',
         },
+        {
+          title: 'Flutter Web',
+          href: '/guides/dart/flutter',
+        },
       ],
     },
   ],
